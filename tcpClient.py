a1bc7889f955ff65eb1bba457e319ea9dcaccc0b
bhuynh <brandonhuynh@umail.ucsb.edu>
2017-05-25 02:03:54 -0700

    Protobuf based messaging protocol
    Implemented image file transfer.
    Added test data.
    
    Note: You should delete generated protobuf code soon...

--- tcpClient.py
+++ tcpClient.py
@@ -1,17 +1,51 @@
 import socket
+import struct
 import sys
+import os
+
+import proto.mapserver_pb2 as server_pb
 
 HOST, PORT = "localhost", 9999
-data = " ".join(sys.argv[1:])
 
-# Create a socket (SOCK_STREAM means a TCP socket)
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-    # Connect to server and send data
-    sock.connect((HOST, PORT))
-    sock.sendall(bytes(data + "\n", "utf-8"))
+if __name__ == '__main__':
+	if len(sys.argv) != 2:
+		print("No Image File Provided")
+		sys.exit(-1)
 
-    # Receive data from the server and shut down
-    received = str(sock.recv(1024), "utf-8")
+	fpath = sys.argv[1]
 
-print("Sent:     {}".format(data))
-print("Received: {}".format(received))
+	if not os.path.isfile(fpath):
+		print("File not found")
+		sys.exit(-1)
+
+	fname = os.path.basename(fpath)
+	name, ext = os.path.splitext(fname)
+
+	# Create Message
+	msg = server_pb.MapserverMessage()
+	msg.client_type = server_pb.MapserverMessage.IMAGE;
+	msg.client_id = 1993
+	msg.image_info.name = name
+	msg.image_info.ext = ext
+
+	with open(fpath, "rb") as f:
+		image_data = bytearray(f.read())
+	msg.image_info.byte_size = len(image_data)
+
+	msg_size = msg.ByteSize()
+	data = bytearray(struct.pack('!i', msg_size))
+	data.extend(bytearray(msg.SerializeToString()))
+	data.extend(image_data)
+
+	# Create a socket (SOCK_STREAM means a TCP socket)
+	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+	    # Connect to server and send data
+	    sock.connect((HOST, PORT))
+	   # sock.sendall(bytes("hello"))
+	    sock.sendall(data)
+
+	    # Receive data from the server and shut down
+	    received = str(sock.recv(1024), "utf-8")
+
+	print("Sent:     {}".format(data))
+	print("Received: {}".format(received))
--- tcpServer.py
+++ tcpServer.py
@@ -1,20 +1,53 @@
 import socketserver
+import struct
+import os
 
-class MyTCPHandler(socketserver.BaseRequestHandler):
+import proto.mapserver_pb2 as server_pb
+
+IMAGE_DATA = 'image_data'
+HOLOLENS_DATA = 'hololens_data'
+
+class MapserverTCPHandler(socketserver.BaseRequestHandler):
 
     def handle(self):
         # self.request is the TCP socket connected to the client
-        self.data = self.request.recv(1024).strip()
-        print("{} wrote:".format(self.client_address[0]))
-        print(self.data)
-        # just send back the same data, but upper-cased
-        self.request.sendall(self.data.upper())
+        self.data = self.request.recv(4)
+        msg_size = struct.unpack('!i', self.data)[0]
+        self.handleMessage(msg_size)
+
+        print("Received message from {}".format(self.client_address[0]))
+        self.request.sendall("OK".encode('utf-8'))
+
+    def handleMessage(self, msg_size):
+        self.data = self.request.recv(msg_size)
+        self.msg = server_pb.MapserverMessage()
+        self.msg.ParseFromString(self.data)
+        print(self.msg)
+
+        if self.msg.client_type == server_pb.MapserverMessage.IMAGE:
+            self.writeJPG()
+
+    def writeJPG(self):
+        fname = self.msg.image_info.name + self.msg.image_info.ext
+        fpath = os.path.join(IMAGE_DATA, fname)
+
+        if os.path.isfile(fpath):
+            os.remove(fpath)
+
+        remaining = self.msg.image_info.byte_size
+        with open(fpath, "ab") as f:
+            while True:
+                img_data = self.request.recv(4096)
+                f.write(img_data)
+                remaining = remaining - len(img_data)
+                if remaining <= 0:
+                    break;
 
 if __name__ == "__main__":
     HOST, PORT = "0.0.0.0", 9999
 
     # Create the server, binding to localhost on port 9999
-    server = socketserver.TCPServer((HOST, PORT), MyTCPHandler)
+    server = socketserver.TCPServer((HOST, PORT), MapserverTCPHandler)
 
     # Activate the server; this will keep running until you
     # interrupt the program with Ctrl-C
