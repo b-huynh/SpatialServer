5bcd03c9dadd523cdd6ef7008492f718a734c13d
animekraxe <yukibhuynh@gmail.com>
2017-07-26 00:29:50 +0200

    Configuration file. To be used for new rest api and upcoming changes.

---
+++ hsm/config.py
@@ -0,0 +1,15 @@
+# Spatial server configuration settings
+
+# Use this flag for adding debug settings!
+DEBUG = False
+
+# Filesystem root directory
+FS_ROOT = '/data/spatial_fs'
+
+# Used for exposing TCP based api server (for real time communications)
+TCP_API_HOST = '0.0.0.0'
+TCP_API_PORT = 8101
+
+# This is used for both rest api and management webapps. Should separate.
+REST_API_HOST = '0.0.0.0'
+REST_API_PORT = 5000
