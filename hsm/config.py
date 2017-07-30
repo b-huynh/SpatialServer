65f0d538e72675464cffd28e8ecef8517450828f
animekraxe <yukibhuynh@gmail.com>
2017-07-30 21:19:55 +0200

    Add default client key with test client key

--- hsm/config.py
+++ hsm/config.py
@@ -13,3 +13,9 @@ TCP_API_PORT = 8101
 # This is used for both rest api and management webapps. Should separate.
 REST_API_HOST = '0.0.0.0'
 REST_API_PORT = 5000
+
+
+# HTTP Related configurations
+
+# TODO: Client probably should be separated from server config options...
+CLIENT_API_KEY = 'fc1431e272194e33ae924a090749a49d'
