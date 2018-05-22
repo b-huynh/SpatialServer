42fe51bec555af0de243bc55c303a723de634d2e
John Alexander <jalexander.qed@gmail.com>
2018-05-22 09:39:46 -0700

    Create system for retrying reconstruction

--- django/sfm/processing_manager.py
+++ django/sfm/processing_manager.py
@@ -4,6 +4,7 @@ import os
 import json
 
 from .sfm_processing import SfmProcessor
+from upload.models import ImageSet
 
 
 potree_exec = '/home/jalexander/software/PotreeConverter/build/PotreeConverter/PotreeConverter'
@@ -14,25 +15,10 @@ def unzip_and_process(filename, image_set):
         os.makedirs(working_dir)
     img_dir = image_set.get_img_dir()
 
-    set_data = json.loads(image_set.data)
     if image_set.status != 'uploaded':
         return
 
-    args = [
-        'unzip',
-        '-j',
-        '-d',
-        img_dir,
-        filename
-    ]
-
-    try:
-        output = subprocess.check_output(args, stderr=subprocess.STDOUT)
-        os.remove(filename)
-    except subprocess.CalledProcessError as err:
-        print('Error unzipping file:', err.returncode)
-        print(bytes.decode(err.output))
-        return
+    unzip_image_set(filename, image_set)
 
     image_set.status = 'unzipped'
     image_set.save()
@@ -58,6 +44,65 @@ def unzip_and_process(filename, image_set):
     print('Finished HTML page generation with Potree')
     image_set.status = 'potree'
     image_set.save()
+
+
+def check_status_and_process(ident):
+    image_set = ImageSet.objects.get(ident=ident)
+    if image_set.status == 'uploaded':
+        print('Cannot handle unzipping image sets')
+
+    processor = SfmProcessor(sfm_source=image_set.get_img_dir(), sfm_dest=image_set.get_working_dir(),
+                             html_dest=image_set.get_html_dir(), potree_executable=potree_exec)
+
+    if image_set.status == 'unzipped':
+        point_file = image_set.get_pointcloud_file()
+        if os.path.isfile(point_file):
+            image_set.status = 'reconstructed'
+            image_set.save()
+        else:
+            print('Starting Colmap reconstruction')
+            processor.run_colmap()
+            print('Finished Colmap reconstruction')
+
+            image_set.status = 'reconstructed'
+            image_set.save()
+
+    if image_set.status == 'reconstructed':
+        html_file = image_set.get_view_html()
+        if os.path.isfile(html_file):
+            image_set.status = 'potree'
+            image_set.save()
+        else:
+            html_dir = image_set.get_html_dir()
+            if not os.path.exists(html_dir):
+                os.makedirs(html_dir)
+
+            processor.convert_point_cloud()
+            processor.convert_dense_ply_file()
+            print('Finished model file conversion')
+            processor.run_potree_convert()
+            print('Finished HTML page generation with Potree')
+            image_set.status = 'potree'
+            image_set.save()
+
+
+
+def unzip_image_set(filename, image_set):
+    args = [
+        'unzip',
+        '-j',
+        '-d',
+        image_set.get_img_dir(),
+        filename
+    ]
+
+    try:
+        output = subprocess.check_output(args, stderr=subprocess.STDOUT)
+        os.remove(filename)
+    except subprocess.CalledProcessError as err:
+        print('Error unzipping file:', err.returncode)
+        print(bytes.decode(err.output))
+        return
 
 
 def start_thread_processing_on_zip(filename, image_set):
--- django/upload/models.py
+++ django/upload/models.py
@@ -12,8 +12,21 @@ class ImageSet(models.Model):
     def get_working_dir(self):
         return os.path.join('sfm_files', self.ident)
 
+    def get_dense_dir(self):
+        return os.path.join(self.get_working_dir(), 'dense')
+
+    def get_latest_dense_dir(self):
+        subdir = sorted(os.listdir(self.get_dense_dir()))[-1]
+        return os.path.join(self.get_dense_dir(), subdir)
+
+    def get_pointcloud_file(self):
+        return os.path.join(self.get_latest_dense_dir(), 'fused.ply')
+
     def get_img_dir(self):
         return os.path.join(self.get_working_dir(), 'img')
 
     def get_html_dir(self):
         return os.path.join('potree', self.ident)
+
+    def get_view_html(self):
+        return os.path.join(self.get_html_dir(), 'view.html')
